92d99d6a4407cda810716ef7d2f62708ec767e22
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-08-27 11:37:58 +0500

    Animated and reusable components

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import AnimatedText from '@/components/AnimatedText'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <div className='h-screen bg-blue-500' />
+      <AnimatedText />
+      <div className='h-screen bg-blue-500' />
+    </>
+  )
+}
+
+export default page
---
+++ src/components/AnimatedText.jsx
@@ -0,0 +1,38 @@
+"use client"
+import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { SplitText } from 'gsap/all'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+const AnimatedText = () => {
+    useGSAP(() => {
+        const splitText = SplitText.create(".tagetText",
+            { type: "chars" }
+        )
+        const tl = gsap.timeline(
+            {
+                scrollTrigger: {
+                    trigger: ".targetDiv",
+                    start: "top top",
+                    toggleActions: "play none none reverse",
+                    markers: true,
+                }
+            }
+        )
+        tl.from(splitText.chars, {
+            opacity: 0,
+            yPercent: 100,
+            stagger: 0.05,
+            ease: "back.out(1.7)",
+        })
+    })
+    return (
+        <div className='targetDiv relative h-screen bg-black flex flex-col justify-center items-center'>
+            <h1 className='tagetText text-9xl font-bold text-white'>Yasir Ali</h1>
+        </div>
+    )
+}
+
+export default AnimatedText
