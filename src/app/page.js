fb7877b8e9000cb191a7f5585651bf342a4a9a93
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-08-27 12:17:52 +0500

    Entry Point

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
-import AnimatedText from '@/components/AnimatedText'
 import React from 'react'
-
+import AnimatedText from '@/components/AnimatedText'
+import InfiniteScrollAnimation from '@/components/InfiniteScrollAnimation'
 const page = () => {
   return (
     <>
       <div className='h-screen bg-blue-500' />
       <AnimatedText />
       <div className='h-screen bg-blue-500' />
+      <InfiniteScrollAnimation />
     </>
   )
 }
